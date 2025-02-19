902dd8fa6719f449f29b8f9cdf4f04a675ebc729
Simon Kilby <simon.kilby@aecom.com>
2025-02-19 17:29:07 +0000

    Add in reformatting fo 2018 English region projections

--- reformat_forecast_population_input_data.py
+++ reformat_forecast_population_input_data.py
@@ -7,7 +7,6 @@ import land_use.preprocessing as pp
 
 
 FORECAST_YEARS = [2023, 2028, 2033, 2038, 2043, 2048]
-ENGLISH_REGIONS_TO_CODES = ["", ""]
 BASE_YEAR = 2023
 
 POPULATION_DIR = Path(r"I:\NorMITs Land Use\2023\import\ONS\forecasting\pop_projs")
@@ -15,9 +14,120 @@ POPULATION_DIR = Path(r"I:\NorMITs Land Use\2023\import\ONS\forecasting\pop_proj
 
 # %%
 def main():
+    process_2018_projections()
     process_2021_22_projections()
 
 
+# %%
+def process_2018_projections() -> None:
+    # Note this is for england only
+    england_2018_forecast_path = (
+        POPULATION_DIR / "2018_based_england_regions_pop_projections.xlsx"
+    )
+    male_df = process_sheet(
+        path_in=england_2018_forecast_path, sheet_name="Males", sex="male"
+    )
+    female_df = process_sheet(
+        path_in=england_2018_forecast_path, sheet_name="Females", sex="female"
+    )
+
+    df = pd.concat([male_df, female_df])
+
+    df = df.reset_index()
+
+    output_years = [year for year in FORECAST_YEARS if year in df]
+
+    for year in output_years:
+
+        df_wide = pd.pivot(df, index=["ntem_age", "g"], columns=["GOR"], values=year)
+
+        filestem = f"2018_based_english_regions_pop_projections_{year}"
+
+        pp.save_preprocessed_hdf(
+            source_file_path=POPULATION_DIR / f"{filestem}.hdf", df=df_wide
+        )
+
+
+def process_sheet(path_in: Path, sheet_name: str, sex: str) -> pd.DataFrame:
+
+    df = pd.read_excel(path_in, sheet_name=sheet_name, skiprows=6)
+
+    region_correspondence = fetch_region_correspondence()
+
+    region_codes = region_correspondence["RGN21CD"]
+
+    df = df.rename(columns={"CODE": "GOR"})
+
+    df = df[df["GOR"].isin(region_codes)]
+
+    # have to limit forecast years to those available
+    available_years = [year for year in FORECAST_YEARS if year in df.columns]
+
+    keep_cols: list[int | str] = ["GOR", "AGE GROUP"]
+    keep_cols.extend(available_years)
+
+    df = df[keep_cols]
+
+    # get lowest age
+    df["from_age"] = df["AGE GROUP"].str.split("-").str[0]
+
+    # fix 15-19 issue mapping to two categories in ratio 1:4
+    df_exc_15 = df[df["from_age"] != "15"].copy()
+    df_15 = df[df["from_age"] == "15"].copy()
+
+    df_16 = df[df["from_age"] == "15"].copy()
+
+    df_15[available_years] = df_15[available_years] / 0.2
+    df_16[available_years] = df_16[available_years] / 0.8
+
+    df = pd.concat([df_exc_15, df_15, df_16])
+
+    # don't forget about 90+
+    df["from_age"] = df["from_age"].str.replace("+", "")
+
+    # drop All Ages
+    df = df[df["from_age"] != "All ages"]
+
+    df["from_age"] = df["from_age"].astype(int)
+
+    df = find_age_ntem_enum(df=df, age_col="from_age")
+
+    df = df.drop(columns=["AGE GROUP", "from_age"])
+
+    df = df.groupby(["GOR", "ntem_age"]).sum()
+
+    if sex == "male":
+        df["g"] = 1
+    elif sex == "female":
+        df["g"] = 2
+    else:
+        raise ValueError(f"unable to allocate g segment for {sex}")
+
+    return df
+
+
+# %%
+def find_age_ntem_enum(df: pd.DataFrame, age_col: str = "age") -> pd.DataFrame:
+    df.loc[df[age_col] < 16, "ntem_age"] = 1  # aged 15 years and under
+    df.loc[(df[age_col] >= 16) & (df[age_col] <= 74), "ntem_age"] = (
+        2  # aged 16 to 74 years
+    )
+    df.loc[df[age_col] >= 75, "ntem_age"] = 3  # aged 75 and over
+    df["ntem_age"] = df["ntem_age"].astype(int)
+    return df
+
+
+def fetch_region_correspondence() -> pd.DataFrame:
+
+    return pd.read_csv(
+        Path(
+            r"I:\NorMITs Land Use\2023\import\ONS\Correspondence_lists",
+            "GOR2021_CD_NM_EWS.csv",
+        )
+    )
+
+
+# %%
 def process_2021_22_projections() -> None:
 
     country_forecasts = {}
@@ -88,18 +198,16 @@ def process_to_segmentations(path_in: Path) -> pd.DataFrame:
 
 
 # %% region approach
-
-# if region approach with mulitple columns for the data then go down this path
-
-# note that we duplicate the values for england to each region, not ideal but
-# as only going to look for growth from 2023 should be fine as long
-# as we don't use this value directly, and do it consistently for the base and future year
-
-
 def create_2022_regions_df(
     country_forecasts: dict[str, pd.DataFrame],
     region_codes: list[str],
 ) -> pd.DataFrame:
+
+    # if region approach with multiple columns for the data then go down this path
+
+    # note that we duplicate the values for england to each region, not ideal but
+    # as only going to look for growth from 2023 should be fine as long
+    # as we don't use this value directly, and do it consistently for the base and future year
 
     region_list = []
     for region_code in region_codes:
@@ -134,13 +242,6 @@ def save_2022_to_hdfs(regions_df: pd.DataFrame) -> None:
         pp.save_preprocessed_hdf(
             source_file_path=POPULATION_DIR / f"{filestem}.hdf", df=df_wide
         )
-
-
-# The other key one is
-# 2018_based_england_regions_pop_projections.xlsx
-# which though older than above gives values by English GOR, which we can
-# use to split down the English population 2021 forecast in future years
-# here we will just get the values out.
 
 # %%
 if __name__ == "__main__":
