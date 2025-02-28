0d4349f4da00324e74fc41eac4c0fd71ef0dbae3
Simon Kilby <simon.kilby@aecom.com>
2025-02-28 14:58:23 +0000

    Add in soc adjustment to forecasting population

--- forecast_population.py
+++ forecast_population.py
@@ -27,28 +27,29 @@ forecast_year = 2043
 
 # %%
 base_emp_dir = Path(r"F:\Deliverables\Land-Use\241213_Population\02_Final Outputs")
+soc_dir = Path(
+    r"I:\NorMITs Land Use\2023\import\Labour Market and Skills\LMS_SOC\preprocessing"
+)
 
 OUTPUT_DIR = Path(r"F:\Working\Land-Use\OUTPUTS_forecast_population")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
 # Define whether to output intermediate outputs, recommended to not output loads if debugging
-generate_summary_outputs = True #bool(config["output_intermediate_outputs"])
+generate_summary_outputs = True  # bool(config["output_intermediate_outputs"])
 
-LOGGER = lu_logging.configure_logger(OUTPUT_DIR / OutputLevel.SUPPORTING, log_name='employment')
+LOGGER = lu_logging.configure_logger(
+    OUTPUT_DIR / OutputLevel.SUPPORTING, log_name="employment"
+)
 
 
 # %%
 
 
 def process_region(gor: str):
-    read_in_files(gor=gor)
-
-
-def read_in_files(gor: str):
     # --- Step 0 --- #
     # read in the currently hard coded but switch to config
 
-    LOGGER.info("Importing data ") # eventually from the config file
+    LOGGER.info(f"Importing data for {gor}")  # eventually from the config file
 
     if gor == "Scotland":
         geographical_level = "SCOTLANDRGN"
@@ -119,25 +120,43 @@ def read_in_files(gor: str):
         geography_subset=geographical_subset,
     )
 
+    soc_base_path = soc_dir / f"LMS_SOC_Occ_T1_{base_year}.hdf"
+
+    soc_base = data_processing.read_dvector_data(
+        file_path=soc_base_path,
+        geographical_level="RGN2021",
+        input_segments=["soc"],
+        geography_subset=gor,
+    )
+
+    soc_forecast_path = soc_dir / f"LMS_SOC_Occ_T1_{forecast_year}.hdf"
+
+    soc_forecast = data_processing.read_dvector_data(
+        file_path=soc_forecast_path,
+        geographical_level="RGN2021",
+        input_segments=["soc"],
+        geography_subset=gor,
+    )
+
     filepath = base_emp_dir / f"Output P11_{gor}.hdf"
     p11 = DVector.load(filepath)
 
     # --- Step 1 --- #
     # Prepare base files into forecasting segmentations
-    LOGGER.info('--- Step 1 ---')
-    LOGGER.info('Prepare base files into forecasting segmentations')
+    LOGGER.info("--- Step 1 ---")
+    LOGGER.info("Prepare base files into forecasting segmentations")
 
     # switch p11's age segmentation from age_9 to age_ntem
     p11_ntem_age = p11.add_segments(new_segs=["age_ntem"])
 
     p11_ntem_age = p11_ntem_age.aggregate(
-        segs=[seg for seg in p11_ntem_age.data.index.names if seg != 'age_9']
+        segs=[seg for seg in p11_ntem_age.data.index.names if seg != "age_9"]
     )
 
     # --- Step 2 --- #
     # Calculate the population growth factors
-    LOGGER.info('--- Step 2 ---')
-    LOGGER.info('Calculate the population growth factors')
+    LOGGER.info("--- Step 2 ---")
+    LOGGER.info("Calculate the population growth factors")
 
     # forecast corrections
     uplift_base_year_factor = dv_national_2022_base / dv_national_2018_base
@@ -159,6 +178,9 @@ def read_in_files(gor: str):
 
     pop_targets = p11_age_ntem_g_gor * adj_growth_factor
 
+    # --- Step 3 --- #
+    # Apply the IPF to targets based on age and gender
+
     rebalanced_p11, summary, differences = data_processing.apply_ipf(
         seed_data=p11_ntem_age,
         target_dvectors=[pop_targets],
@@ -167,11 +189,96 @@ def read_in_files(gor: str):
 
     data_processing.save_output(
         output_folder=OUTPUT_DIR,
-        output_reference=f'Output p11_ge_g_{gor}',
+        output_reference=f"Output p11_ge_g_{gor}",
         dvector=rebalanced_p11,
-        dvector_dimension='people',
-        output_level=OutputLevel.INTERMEDIATE
+        dvector_dimension="people",
+        output_level=OutputLevel.INTERMEDIATE,
     )
+
+    # --- Step 4 --- #
+    # Calculate population numbers (post initial IPF) excluding soc 4
+
+    rebalanced_p11_gor = rebalanced_p11.translate_zoning(soc_base.zoning_system)
+
+    rebalanced_p11_soc_totals = rebalanced_p11_gor.aggregate(["soc"])
+
+    rebalanced_p11_soc_totals_exc_4 = rebalanced_p11_soc_totals.filter_segment_value(
+        "soc", [1, 2, 3]
+    )
+
+    rebalanced_p11_soc_totals_exc_4_totals = (
+        rebalanced_p11_soc_totals_exc_4.add_segments(["total"]).aggregate(["total"])
+    )
+
+    rebalanced_p11_soc_totals_exc_4_totals = (
+        rebalanced_p11_soc_totals_exc_4_totals.add_segments(
+            ["soc"]
+        ).filter_segment_value("soc", [1, 2, 3])
+    )
+
+    # --- Step 5 --- #
+    # Calculate the new SOC splits
+
+    # soc 4 is excluded throughout as do not have a forecast for this segment
+    soc_base_totals = (
+        soc_base.add_segments(["total"])
+        .aggregate(["total"])
+        .add_segments(["soc"])
+        .filter_segment_value("soc", [1, 2, 3])
+    )
+
+    soc_forecast_totals = (
+        soc_forecast.add_segments(["total"])
+        .aggregate(["total"])
+        .add_segments(["soc"])
+        .filter_segment_value("soc", [1, 2, 3])
+    )
+
+    # calcate the soc splits from the forecast data
+    soc_base_perc = soc_base / soc_base_totals
+    soc_forecast_perc = soc_forecast / soc_forecast_totals
+
+    # work out the change in per splits
+    soc_splits_change = soc_forecast_perc - soc_base_perc
+
+    p11_gor = p11.translate_zoning(soc_base.zoning_system)
+
+    p11_gor_soc = p11_gor.aggregate(["soc"]).filter_segment_value("soc", [1, 2, 3])
+    p11_soc_totals = (
+        p11_gor_soc.add_segments(["total"])
+        .aggregate(["total"])
+        .add_segments(["soc"])
+        .filter_segment_value("soc", [1, 2, 3])
+    )
+
+    soc_p11_perc = p11_gor_soc / p11_soc_totals
+
+    # work out the new targets splits 
+    # TODO: add in check to make sure this isn't negative, and if so need to do something about it.
+    soc_target_perc = soc_p11_perc + soc_splits_change
+
+    # the totals here should be the pop_targets without soc 4
+    soc_targets = (soc_target_perc * rebalanced_p11_soc_totals_exc_4_totals).aggregate(
+        ["soc"]
+    )
+
+    # --- Step 6 --- #
+    # Apply the IPF to targets based on age, gender and soc
+
+    rebalanced_age_g_soc_p11, summary, differences = data_processing.apply_ipf(
+        seed_data=p11_ntem_age,
+        target_dvectors=[pop_targets, soc_targets],
+        cache_folder=constants.CACHE_FOLDER,
+    )
+
+    data_processing.save_output(
+        output_folder=OUTPUT_DIR,
+        output_reference=f"Output p11_age_g_soc_{gor}",
+        dvector=rebalanced_age_g_soc_p11,
+        dvector_dimension="people",
+        output_level=OutputLevel.INTERMEDIATE,
+    )
+
 
 # # takes a while to run. So suggest this is run only when needed
 # for gor in constants.GORS:
